870f08ead154bdd94cb67e73c66a575facab63a6
MatteoDosoo <matterove2@gmail.com>
2024-09-10 09:28:44 +0200

    Update script.js

--- module4/js/script.js
+++ module4/js/script.js
@@ -1,9 +1,9 @@
-var names = ["Matteo", "Kobe", "Ale", "Tom", "Andy", "Aldo"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var name in names) {
   var firstLetter = names[name].charAt().toLowerCase();
 
-  if (firstLetter == 'a') {
+  if (firstLetter == 'j') {
     byeSpeaker.speak(names[name]);
   } else {
     helloSpeaker.speak(names[name]);
